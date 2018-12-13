208438765158a1d92d3d1fba660287f55b4c362b
starfish01 <patrick.labes@gmail.com>
2018-12-13 21:18:34 +1000

    evening lessons

--- Section 2 - Widgets/flutter_course/lib/main.dart
+++ Section 2 - Widgets/flutter_course/lib/main.dart
@@ -1,16 +1,21 @@
 import 'package:flutter/material.dart';
 
-main() {
-  runApp(MyApp());
-}
+import './product_manager.dart';
+
+// void main() {
+//   runApp(MyApp());
+// }
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  build(context) {
+  @override
+  Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('Easy List'),
         ),
+        body: ProductManager('Food Tester'),
       ),
     );
   }

d4a676b8a62250b44b30a95fa578022fd187aa7e
starfish01 <patrick.labes@gmail.com>
2018-12-17 06:38:00 +1000

    morning lessons - starting navigation section

---
+++ Section 6 - Navigation/sectionapp/lib/products.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class Products extends StatelessWidget {
+  final List<String> products;
+
+  Products([this.products = const []]) {
+    print('[Products Widget] Constructor');
+  }
+
+  Widget _buildProductItem(BuildContext context, int index) {
+    return Card(
+      child: Column(
+        children: <Widget>[
+          Image.asset('assets/food.jpg'),
+          Text(products[index])
+        ],
+      ),
+    );
+  }
+
+  Widget _buildProductList() {
+    Widget productCards = Center(child: Text('No Products Found'));
+
+    if(products.length > 0){
+      productCards = ListView.builder(
+      itemBuilder: _buildProductItem,
+      itemCount: products.length);
+    }
+
+    return productCards;
+  }
+  
+
+  @override
+  Widget build(BuildContext context) {
+    print('[Products Widget] build()');
+
+    return _buildProductList();
+  }
+}

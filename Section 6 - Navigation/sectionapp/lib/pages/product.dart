fb21c42232725349d22aab0f6903c7674ed7ce0c
Patrick Labes <patrick.labes@gmail.com>
2018-12-18 14:23:24 +1000

    Lunch time lessons passing Maps

--- Section 6 - Navigation/sectionapp/lib/pages/product.dart
+++ Section 6 - Navigation/sectionapp/lib/pages/product.dart
@@ -1,24 +1,31 @@
 import 'package:flutter/material.dart';
 
 class ProductPage extends StatelessWidget {
+
+  final String title;
+  final String imageUrl;
+
+  ProductPage(this.title,this.imageUrl);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text('Product detail name'),
+          title: Text(title),
         ),
         body: Column(
           // mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
-            Image.asset('assets/food.jpg'),
-            Container(padding: EdgeInsets.all(10.0),child:
-            Text('details')),
-            Container(padding: EdgeInsets.all(10.0),child:
-            RaisedButton(
-              child: Text('back'),
-              onPressed: () => Navigator.pop(context),
-            ))
+            Image.asset(imageUrl),
+            Container(padding: EdgeInsets.all(10.0), child: Text(title)),
+            Container(
+                padding: EdgeInsets.all(10.0),
+                child: RaisedButton(
+                  color:Theme.of(context).accentColor,
+                  child: Text('back'),
+                  onPressed: () => Navigator.pop(context),
+                ))
           ],
         ));
   }
--- Section 6 - Navigation/sectionapp/lib/product_control.dart
+++ Section 6 - Navigation/sectionapp/lib/product_control.dart
@@ -10,7 +10,7 @@ class ProductControl extends StatelessWidget {
       return RaisedButton(
             color: Theme.of(context).primaryColor,
             onPressed: () {
-              addProduct('Sweets');
+              addProduct({'title':'Choclate','imageUrl':'assets/food.jpg'});
             },
             child: Text('Add Product'),
           );

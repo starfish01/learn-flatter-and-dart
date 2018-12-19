cb38e93577efca8b2b4a237723f29c9e74e6497d
Patrick Labes <patrick.labes@gmail.com>
2018-12-19 14:29:20 +1000

    finished the navigation section of the lessons - Just need to write the documentaion from what i have learned now

--- Section 6 - Navigation/sectionapp/lib/pages/product_create.dart
+++ Section 6 - Navigation/sectionapp/lib/pages/product_create.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
 
-class ProductCreatePage extends StatelessWidget{
+class ProductCreatePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text('Create a Product'),
+      child: RaisedButton(
+          child: Text('Save'),
+          onPressed: () {
+            showModalBottomSheet(
+                context: context,
+                builder: (BuildContext context) {
+                  return Center(child: Text('This is a model'));
+                });
+          }),
     );
   }
-
 }

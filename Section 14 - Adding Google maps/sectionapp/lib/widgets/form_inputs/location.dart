18f6bb0fa2582cc0ce095e69aca57a8f649e124d
starfish01 <patrick.labes@gmail.com>
2019-01-08 21:25:50 +1000

    learning about google maps

---
+++ Section 14 - Adding Google maps/sectionapp/lib/widgets/form_inputs/location.dart
@@ -0,0 +1,94 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+
+import 'package:map_view/map_view.dart';
+import 'package:http/http.dart' as http;
+
+import '../helpers/ensure_visible.dart';
+
+class LocationInput extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _LocationInputState();
+  }
+}
+
+class _LocationInputState extends State<LocationInput> {
+  Uri _staticMapUri;
+  final FocusNode _addressInputFocusNode = FocusNode();
+  final TextEditingController _addressInputController = TextEditingController();
+
+  @override
+  void initState() {
+    _addressInputFocusNode.addListener(_updateLocation);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _addressInputFocusNode.removeListener(_updateLocation);
+    super.dispose();
+  }
+
+  void getStaticMap(String address) async {
+    if (address.isEmpty) {
+      return;
+    }
+    final Uri uri = Uri.https(
+      'maps.googleapis.com',
+      '/maps/api/geocode/json',
+      {'address': address, 'key': 'AIzaSyADQ5Fu-OwDVKjaM5pkkLWoVa1wteIaNgU'},
+    );
+    final http.Response response = await http.get(uri);
+    final decodedResponse = json.decode(response.body);
+
+    print(decodedResponse);
+
+    final formattedAddress = decodedResponse['results'][0]['formatted_address'];
+    final coords = decodedResponse['results'][0]['geometry']['location'];
+
+    final StaticMapProvider staticMapViewProvider =
+        StaticMapProvider('AIzaSyADQ5Fu-OwDVKjaM5pkkLWoVa1wteIaNgU');
+    final Uri staticMapUri = staticMapViewProvider.getStaticUriWithMarkers(
+        [Marker('position', 'Position', coords['lat'], coords['lng'])],
+        center: Location(coords['lat'], coords['lng']),
+        width: 500,
+        height: 300,
+        maptype: StaticMapViewType.roadmap);
+    setState(() {
+      _addressInputController.text = formattedAddress;
+      _staticMapUri = staticMapUri;
+    });
+  }
+
+  void _updateLocation() {
+    if (!_addressInputFocusNode.hasFocus) {
+      getStaticMap(_addressInputController.text);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        EnsureVisibleWhenFocused(
+          focusNode: _addressInputFocusNode,
+          child: TextFormField(
+            focusNode: _addressInputFocusNode,
+            controller: _addressInputController,
+            decoration: InputDecoration(labelText: 'Address'),
+          ),
+        ),
+        SizedBox(
+          height: 10.0,
+        ),
+        FadeInImage(
+              image: NetworkImage(_staticMapUri.toString()),
+              height: 300.0,
+              fit: BoxFit.cover,
+              placeholder: AssetImage('assets/food.jpg'),
+            ),
+      ],
+    );
+  }
+}
